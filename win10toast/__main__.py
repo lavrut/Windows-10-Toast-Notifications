3ae595d3c68ce1732983c441013568c673e80589
LR <32437530+lavrut@users.noreply.github.com>
2020-05-03 16:15:16 -0400

    Update __main__.py

--- win10toast/__main__.py
+++ win10toast/__main__.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
     # Example
     toaster = ToastNotifier()
     toaster.show_toast(
-        "Hello World!!!",
-        "Python is 10 seconds awsm!",
+        "You have entered an invalid address!",
+        "Please check for any errors",
         duration=10)
-    toaster.show_toast(
-        "Example two",
-        "This notification is in it's own thread!",
-        icon_path=None,
-        duration=5,
-        threaded=True
-        )
+#     toaster.show_toast(
+#         "Example two",
+#         "This notification is in it's own thread!",
+#         icon_path=None,
+#         duration=5,
+#         threaded=True
+#         )
     # Wait for threaded notification to finish
     while toaster.notification_active(): time.sleep(0.1)
